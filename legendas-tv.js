a6ec8868d2fc482f600c9adf090da953ae226150
Renan Pasqueto Gomes <renan.pasqueto@gmail.com>
2017-06-07 00:45:27 -0300

    Fetch synopsis.

--- legendas-tv.js
+++ legendas-tv.js
@@ -2,36 +2,80 @@ var request = require('request');
 
 var legendasTv = function () {
 
+    var _url = 'http://legendas.tv';
     var _movies = [];
+    
+    var Movie = (function () {
+        var id = 0;
 
-    var Movie = function (name, release, date) {
-        this.name = name;
-        this.release = release;
-        this.date = date;
-    };
-
+        return function (name, release, date, href) {
+            this.id = ++id;
+            this.name = name;
+            this.release = release;
+            this.date = date;
+            this.href = href;
+        };
+    })();
+    
     _movies.bluRay = function () {
         return this.filter(function (movie) {
             return (movie.release.search(/(BluRay)|(BDRip)|(BRRip)/gi) !== -1);
         });
     };
 
-    var _weeklyHighlights = function (rawBody) {
-        var regex = /<div class="item">.*?<span>([^<>]*)<\/span>.*?<div class="tooltip">(?:<p>([^<>]*)<\/p>)(?:<p>([^<>]*)<\/p>){2}/g;
-        
+    _movies.get = function (id) {
+        return this.filter(function (movie) {
+            return movie.id == id;
+        })[0];
+    }
+
+    var _fetchWeeklyHighlights = function (rawBody) {
+        var regex = /<div class="item"><a href=(?:"|')([^"']*)".*?<span>([^<>]*)<\/span>.*?<div class="tooltip">(?:<p>([^<>]*)<\/p>)(?:<p>([^<>]*)<\/p>){2}/g;
         var m;
+
         while ((m = regex.exec(rawBody)) !== null)  {
-            _movies.push(new Movie(m[1].trim(), m[2].trim(), m[3].trim()));
+             m = m.map(function (val) { return val.trim() });
+            _movies.push(new Movie(m[2], m[3], m[4], m[1]));
         }
+    };
+
+    var _fetchSynopsis = function (id, callback) {
+        var movie = _movies.get(id);
+
+        if (movie.synopsis) return callback(movie);
+
+        request(_url + movie.href, function (error, response, body) {
+            if (error) throw error;
+
+            var regex = /<div class="t1"[^>]*>\s{0,}<p>([^<]+)<\/p>/g;
+            var synopsis = regex.exec(body)[1];
+            
+            for (var i in _movies) {
+                if (_movies[i].id === id) {
+                    _movies[i].synopsis = synopsis;
+                    movie = _movies[i];
+                    break;
+                }
+            }
+
+            callback(movie);
+        });
     };
 
     this.onSearchable = function (callback) {
         if (_movies.length) return callback(_movies);
-        request('http://legendas.tv', function (error, response, body) {
+
+        request(_url, function (error, response, body) {
             if (error) throw error;
 
-            _weeklyHighlights(body);
+            _fetchWeeklyHighlights(body);
             callback(_movies);
+        });
+    };
+
+    this.onSynopsisReady = function (id, callback) {
+        this.onSearchable(function () {
+            _fetchSynopsis(id, callback);
         });
     };
 };
