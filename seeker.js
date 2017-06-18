be3435ef0bf741a5a4ab361ad35131bd929b0028
Renan Pasqueto Gomes <renan.pasqueto@gmail.com>
2017-06-17 23:41:10 -0300

    Added a seeker for subtitles.

---
+++ seeker.js
@@ -0,0 +1,44 @@
+var fs = require('fs');
+var config = require('./config');
+var legendasTv = require('./legendas-tv');
+var error = require('./error');
+
+var _today = new Date();
+var _movies = [];
+
+var downloadSubtitle = function (movie) {
+  if (!movie) throw new error.MovieNotFound();
+
+  legendasTv.onDownloadSubtitle(movie.id, function (filename) {
+    console.log(filename + ' downloaded');
+  });
+};
+
+var seekSeries = function () {
+  config.seeker.series.forEach(function (serie, i) {
+    if (_today < new Date(serie.releaseDate)) return;
+    
+    var episodeFound;
+    serie.episodes.forEach(function (episode, j) {
+      
+      try {
+        downloadSubtitle(_movies.find(serie.title, episode)[0]);
+        episodeFound = j;
+      } catch (err) {
+        console.log(serie.title + ' ' + episode + ' ' + 'not found');
+      }
+
+    });
+
+    if (!episodeFound) return;
+    serie.episodes.splice(0, ++episodeFound);
+  });
+
+  fs.writeFile('config.json', JSON.stringify(config));
+}
+
+legendasTv.onReady(function (movies) {  
+  _movies = movies;
+  seekSeries();
+});
+
